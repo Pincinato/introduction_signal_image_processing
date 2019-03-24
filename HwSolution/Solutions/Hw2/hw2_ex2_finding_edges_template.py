f29c24a958d0c9b3824da9543776bd94fd4688a3
pincinato <thiagoh.pincinato@gmail.com>
2019-03-24 21:53:27 +0100

    ex2-2 solved

--- HwSolution/Solutions/Hw2/hw2_ex2_finding_edges_template.py
+++ HwSolution/Solutions/Hw2/hw2_ex2_finding_edges_template.py
@@ -5,6 +5,9 @@ from skimage import color, io
 import matplotlib.pyplot as plt
 plt.rcParams['image.cmap'] = 'gray'
 import pdb
+import math
+
+import functionsEx1 as pinc
 
 # load image
 img = io.imread('bird.jpg')
@@ -17,22 +20,20 @@ img = color.rgb2gray(img)
 # 2.1
 # Gradients
 # define a derivative operator
-dx = ### your code should go here ###
-dy = ### your code should go here ###
-
+dx = np.array([[-1, 0, 1]])
+dy = np.array([[-1, 0, 1]])
 # convolve derivative operator with a 1d gaussian filter with sigma = 1
 # You should end up with 2 1d edge filters,  one identifying edges in the x direction, and
 # the other in the y direction
 sigma = 1
-### your code should go here ###
-gdx = ### your code should go here ###
-gdy = ### your code should go here ###
-
+gaussianFilter = pinc.gauss1d(sigma, 3)
+gdx = pinc.myconv2(gaussianFilter, dx)
+gdy = np.transpose(pinc.myconv2(gaussianFilter, dy))
 
 # 2.2
 # Gradient Edge Magnitude Map
 def create_edge_magn_image(image, dx, dy):
-    # this function created an eddge magnitude map of an image
+    # this function created an edge magnitude map of an image
     # for every pixel in the image, it assigns the magnitude of gradients
     # INPUTS
     # @image  : a 2D image
@@ -40,15 +41,22 @@ def create_edge_magn_image(image, dx, dy):
     # @gdy     : geadient along y axis
     # OUTPUTS
     # @ grad_mag_image  : 2d image same size as image, with the magnitude of gradients in every pixel
-    # @grad_dir_image   : 2d image same size as image, with the direcrion of gradients in every pixel
+    # @grad_dir_image   : 2d image same size as image, with the direction of gradients in every pixel
 
-    ### your code should go here ###
-
+    # Computing mag in x and in y
+    mag_x = np.asarray(pinc.myconv2(image, dx))[:, round(dx.shape[1]/2):image.shape[1] + round(dx.shape[1]/2)]
+    mag_y = np.asarray(pinc.myconv2(image, dy))[round(dy.shape[0]/2):image.shape[0] + round(dy.shape[0]/2), :]
+    # computing magnitude
+    grad_mag_image = np.sqrt(np.power(mag_x,2) + np.power(mag_y,2))
+    # computing angles
+    grad_dir_image = np.angle(mag_x + 1j*mag_y)
+    # making all angles positive
+    grad_dir_image[np.where(grad_dir_image < 0)] = grad_dir_image[np.where(grad_dir_image < 0)] + 2*math.pi
     return grad_mag_image, grad_dir_image
 
 
 # create an edge magnitude image using the derivative operator
-img_edge_mag, img_edge_dir = create_edge_magn_image(img, dx, dy)
+img_edge_mag, img_edge_dir = create_edge_magn_image(img, dx, dy.T)
 
 # show all together
 plt.subplot(121)
@@ -56,6 +64,7 @@ plt.imshow(img)
 plt.axis('off')
 plt.title('Original image')
 plt.subplot(122)
+
 plt.imshow(img_edge_mag)
 plt.axis('off')
 plt.title('Edge magnitude map')
@@ -71,8 +80,14 @@ def make_edge_map(image, dx, dy):
     # OUTPUTS:
     # @ edge maps   : a 3D array of shape (image.shape, 8) containing the edge maps on 8 orientations
 
-    ### your code should go here ###
-
+    (grad_mag_image, grad_dir_image) = create_edge_magn_image(image, dx, dy.T)
+    r = 50
+    edge_maps = np.zeros([image.shape[0], image.shape[1], 8])
+    for i in range(8):
+        minAngle = i*2*math.pi/8 - math.pi/8
+        maxAngle = i*2*math.pi/8 + math.pi/8
+        index = np.where((grad_mag_image > r) & (grad_dir_image > minAngle) & (grad_dir_image < maxAngle))
+        edge_maps[index[0],index[1], i] = 255
     return edge_maps
 
 
@@ -106,7 +121,7 @@ def edge_non_max_suppression(img_edge_mag, edge_maps):
     # @non_max_sup    : 2d image with the non max suppresed pixels
 
     ### your code should go here ###
-
+    non_max_sup = 0
     return non_max_sup
 
 
@@ -128,7 +143,7 @@ def canny_edge(image, sigma=2):
     # @canny_img  : 2d image of size same as image, with the result of the canny edge detection
 
     ### your code should go here ###
-
+    canny_img = 0
     return canny_img
 
 canny_img = canny_edge(img)
